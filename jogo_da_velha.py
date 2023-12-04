cabe35578bf4df1264ec17c986c077147d313769
CaduEC <152208835+CaduEC@users.noreply.github.com>
2023-12-04 14:34:54 -0300

    Update jogo_da_velha.py

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,7 +1,10 @@
+import random
+
 class JogoDaVelha:
     def __init__(self):
         self.tabuleiro = [[' ' for _ in range(3)] for _ in range(3)]
         self.jogador_atual = 'X'
+        self.jogador_maquina = 'O'
 
     def imprimir_tabuleiro(self):
         for linha in self.tabuleiro:
@@ -11,22 +14,22 @@ class JogoDaVelha:
     def fazer_jogada(self, linha, coluna):
         if self.tabuleiro[linha][coluna] == ' ':
             self.tabuleiro[linha][coluna] = self.jogador_atual
-            self.jogador_atual = 'O' if self.jogador_atual == 'X' else 'X'
+            self.jogador_atual, self.jogador_maquina = self.jogador_maquina, self.jogador_atual
         else:
             print("Essa posição já está ocupada. Tente novamente.")
 
-    def verificar_vitoria(self):
+    def verificar_vitoria(self, jogador):
         # Verificar linhas e colunas
         for i in range(3):
-            if self.tabuleiro[i][0] == self.tabuleiro[i][1] == self.tabuleiro[i][2] != ' ':
+            if self.tabuleiro[i][0] == self.tabuleiro[i][1] == self.tabuleiro[i][2] == jogador:
                 return True
-            if self.tabuleiro[0][i] == self.tabuleiro[1][i] == self.tabuleiro[2][i] != ' ':
+            if self.tabuleiro[0][i] == self.tabuleiro[1][i] == self.tabuleiro[2][i] == jogador:
                 return True
 
         # Verificar diagonais
-        if self.tabuleiro[0][0] == self.tabuleiro[1][1] == self.tabuleiro[2][2] != ' ':
+        if self.tabuleiro[0][0] == self.tabuleiro[1][1] == self.tabuleiro[2][2] == jogador:
             return True
-        if self.tabuleiro[0][2] == self.tabuleiro[1][1] == self.tabuleiro[2][0] != ' ':
+        if self.tabuleiro[0][2] == self.tabuleiro[1][1] == self.tabuleiro[2][0] == jogador:
             return True
 
         return False
@@ -38,25 +41,40 @@ class JogoDaVelha:
                     return False
         return True
 
-class JogoDaVelhaModificado(JogoDaVelha):
+class JogoDaVelhaVsMaquina(JogoDaVelha):
     def __init__(self):
         super().__init__()
 
-    def fazer_jogada(self, linha, coluna):
-        super().fazer_jogada(linha, coluna)
-        if self.verificar_vitoria():
-            print(f"Jogador {self.jogador_atual} venceu!")
-        elif self.verificar_empate():
-            print("O jogo empatou.")
-        else:
+    def fazer_jogada_maquina(self):
+        while True:
+            linha = random.randint(0, 2)
+            coluna = random.randint(0, 2)
+            if self.tabuleiro[linha][coluna] == ' ':
+                self.tabuleiro[linha][coluna] = self.jogador_maquina
+                self.jogador_atual, self.jogador_maquina = self.jogador_maquina, self.jogador_atual
+                break
+
+    def jogar(self):
+        while not self.verificar_vitoria(self.jogador_atual) and not self.verificar_empate():
             self.imprimir_tabuleiro()
 
+            if self.jogador_atual == 'X':
+                linha = int(input("Digite a linha da sua jogada (0, 1 ou 2): "))
+                coluna = int(input("Digite a coluna da sua jogada (0, 1 ou 2): "))
+                self.fazer_jogada(linha, coluna)
+            else:
+                print("Vez da máquina:")
+                self.fazer_jogada_maquina()
+
+        self.imprimir_tabuleiro()
+
+        if self.verificar_vitoria('X'):
+            print("Você venceu!")
+        elif self.verificar_vitoria('O'):
+            print("A máquina venceu!")
+        else:
+            print("O jogo empatou.")
 
 # Exemplo de uso
-jogo = JogoDaVelhaModificado()
-
-while not jogo.verificar_vitoria() and not jogo.verificar_empate():
-    jogo.imprimir_tabuleiro()
-    linha = int(input("Digite a linha da sua jogada (0, 1 ou 2): "))
-    coluna = int(input("Digite a coluna da sua jogada (0, 1 ou 2): "))
-    jogo.fazer_jogada(linha, coluna)
+jogo_vs_maquina = JogoDaVelhaVsMaquina()
+jogo_vs_maquina.jogar()
