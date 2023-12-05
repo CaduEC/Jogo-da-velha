78072364ea07883e4d5b6c3e2e6f436aaae3792b
CaduEC <152208835+CaduEC@users.noreply.github.com>
2023-12-05 14:47:58 -0300

    Update jogo_da_velha.py

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,7 +1,45 @@
 import random
 
 class JogoDaVelha:
-    # ... (o restante do código permanece o mesmo)
+    def __init__(self):
+        self.tabuleiro = [[' ' for _ in range(3)] for _ in range(3)]
+        self.jogador_atual = 'O'
+        self.jogador_maquina = 'X'
+
+    def imprimir_tabuleiro(self):
+        for linha in self.tabuleiro:
+            print('|'.join(linha))
+            print('-' * 5)
+
+    def fazer_jogada(self, linha, coluna):
+        if self.tabuleiro[linha][coluna] == ' ':
+            self.tabuleiro[linha][coluna] = self.jogador_atual
+            self.jogador_atual, self.jogador_maquina = self.jogador_maquina, self.jogador_atual
+        else:
+            print("Essa posição já está ocupada. Tente novamente.")
+
+    def verificar_vitoria(self, jogador):
+        # Verificar linhas e colunas
+        for i in range(3):
+            if self.tabuleiro[i][0] == self.tabuleiro[i][1] == self.tabuleiro[i][2] == jogador:
+                return True
+            if self.tabuleiro[0][i] == self.tabuleiro[1][i] == self.tabuleiro[2][i] == jogador:
+                return True
+
+        # Verificar diagonais
+        if self.tabuleiro[0][0] == self.tabuleiro[1][1] == self.tabuleiro[2][2] == jogador:
+            return True
+        if self.tabuleiro[0][2] == self.tabuleiro[1][1] == self.tabuleiro[2][0] == jogador:
+            return True
+
+        return False
+
+    def verificar_empate(self):
+        for linha in self.tabuleiro:
+            for elemento in linha:
+                if elemento == ' ':
+                    return False
+        return True
 
 class JogoDaVelhaVsMaquina(JogoDaVelha):
     def __init__(self):
@@ -16,22 +54,14 @@ class JogoDaVelhaVsMaquina(JogoDaVelha):
                 self.jogador_atual, self.jogador_maquina = self.jogador_maquina, self.jogador_atual
                 break
 
-    def obter_jogada_do_usuario(self):
-        try:
-            linha = int(input("Digite a linha da sua jogada (0, 1 ou 2): "))
-            coluna = int(input("Digite a coluna da sua jogada (0, 1 ou 2): "))
-            return linha, coluna
-        except ValueError:
-            print("Entrada inválida. Por favor, digite um número.")
-            return self.obter_jogada_do_usuario()
-
     def jogar(self):
-        while True:
+        while True:  # Loop externo para permitir jogar novamente
             while not self.verificar_vitoria(self.jogador_atual) and not self.verificar_vitoria('Y') and not self.verificar_empate():
                 self.imprimir_tabuleiro()
 
                 if self.jogador_atual == 'O':
-                    linha, coluna = self.obter_jogada_do_usuario()
+                    linha = int(input("Digite a linha da sua jogada (0, 1 ou 2): "))
+                    coluna = int(input("Digite a coluna da sua jogada (0, 1 ou 2): "))
                     self.fazer_jogada(linha, coluna)
                 else:
                     print("Vez da máquina:")
@@ -40,21 +70,20 @@ class JogoDaVelhaVsMaquina(JogoDaVelha):
             self.imprimir_tabuleiro()
 
             if self.verificar_vitoria('O'):
-                print("Você venceu!")
+                print("Parabéns! Você venceu!")
             elif self.verificar_vitoria('Y'):
-                print("A máquina venceu!")
+                print("Você perdeu. Melhor sorte da próxima vez!")
             else:
-                print("O jogo empatou.")
+                print("O jogo terminou em empate.")
 
-            # Pergunta se o jogador quer jogar novamente
-            jogar_novamente = input("Deseja jogar novamente? (s/n): ").lower()
-            if jogar_novamente != 's':
-                print("Obrigado por jogar. Até a próxima!")
-                break
-            else:
-                # Reinicia o jogo
-                self.tabuleiro = [[' ' for _ in range(3)] for _ in range(3)]
-                self.jogador_atual, self.jogador_maquina = 'O', 'X'
+            # Pergunta se deseja jogar novamente
+            jogar_novamente = input("Deseja jogar novamente? (s/n): ")
+            if jogar_novamente.lower() != 's':
+                break  # Sai do loop externo se não desejar jogar novamente
+
+            # Reinicializa o tabuleiro para uma nova partida
+            self.tabuleiro = [[' ' for _ in range(3)] for _ in range(3)]
+            self.jogador_atual, self.jogador_maquina = 'O', 'X'
 
 # Exemplo de uso
 jogo_vs_maquina = JogoDaVelhaVsMaquina()
